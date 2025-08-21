ab0afd43aa8b5d7ecb9eed55b13ae795a6cb7545
psheshera <sheshera@gmail.com>
2025-08-21 13:53:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -82,5 +82,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+app.add_handler(CommandHandler("summary", summary))
 
 app.run_polling()
